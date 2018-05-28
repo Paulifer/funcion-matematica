97d68c5c945dba7f0f3fa285fe0f0fc2b8a16229
Paulifer <pauli.hua.timm@gmail.com>
2018-05-28 03:11:02 -0400

    añadiendo funciones arrow para sumar y multiplicar

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+//funcion oara sumar dos argumentos
+const add = (num1,num2) => num1+num2;
+//funcion para multiplicar dos argumentos
+const multiply = (num1,num2) => num1*num2;
+//variable para guardar la suma
+const resultAdd = add(9824,777);
+//variable para guardar la multiplicaion de los argumentos
+const resultMultiply = multiply(36325,resultAdd);
+console.log('El resultado es = ' + resultMultiply);
+document.write('El resultado es = ' + resultMultiply);
